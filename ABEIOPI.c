0b6c54e4515d479223aaee75dca0c0690033be0e
balferreira <balferreira@users.noreply.github.com>
2017-02-09 08:02:21 +0000

    Update ABEIOPI.c

--- ABEIOPI.c
+++ ABEIOPI.c
@@ -1,5 +1,5 @@
 /* 
-    Copyright (c) Bruno Ferreira <@gmail.com> 
+    Copyright (c) Bruno Ferreira
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
